c870bde34ad95c6bd333f438badf14a65d652470
LoetRomijn <loetromijn@gmail.com>
2016-01-13 17:18:34 +0100

    First commit

--- file.js
+++ file.js
@@ -1,12 +1,4 @@
-// This file is created by Loet Romijn as part of the NYCDA full-stack webdevelopment course (BSSA Amsterdam) //
-
 console.log("some javascript");
 
 
-function testFunction(){
-	console.log("This is a test");
-}
 
-// Just another change to get comfortable with the git commands to commit and push changes to github // 
-// And another change //
-// To test if my key is working now //
